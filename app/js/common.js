6f7198a712a3bdb6d837092255681c77434300f7
Viktor Andriichuk <avnicolaevich@gmail.com>
2018-01-24 02:13:50 +0200

    add header banner and explorer section

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+    var headerOwlCarousel = $('#headerOwl');
+    // Carousel initialization
+    headerOwlCarousel.owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        margin: 0,
+        animateOut: 'fadeOut',
+        items: 1
+    });
 
 });
