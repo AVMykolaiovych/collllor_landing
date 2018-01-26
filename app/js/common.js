7608927b9fe287050a03a9261b14b861d3537cb0
Viktor Andriichuk <avnicolaevich@gmail.com>
2018-01-26 02:31:35 +0200

    add welcome, steps and terms section

--- app/js/common.js
+++ app/js/common.js
@@ -1,24 +1,65 @@
 $(function() {
 
-    var headerOwlCarousel = $('#headerOwl');
-    // Carousel initialization
-    headerOwlCarousel.owlCarousel({
+    $('#headerOwl').owlCarousel({
         loop: true,
         autoplay: true,
-        autoplayTimeout: 4000,
+        autoplayTimeout: 3000,
         dots: false,
         margin: 0,
         animateOut: 'fadeOut',
         items: 1
     });
-    // Carousel initialization
+
     $('#videoOwl').owlCarousel({
         items:1,
         nav: true,
         navText: '',
+        autoplay: true,
+        autoplayTimeout: 3000,
         merge: true,
         loop: true,
+        smartSpeed: 700,
+        center: true
+    });
+
+    $("#stepsOwl").owlCarousel({
+        items : 5,
+        nav: true,
+        navText: '',
+        margin: 10,
+        slideSpeed : 500,
         smartSpeed: 600,
+        dots: false,
+        responsiveRefreshRate : 200,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2
+            },
+            480:{
+                items: 3
+            },
+            767:{
+                items: 3
+            },
+            992:{
+                items: 4
+            },
+            1200:{
+                items: 5
+            }
+        }
+    });
+
+    $('#welcomeOwl').owlCarousel({
+        items:1,
+        // autoplay: true,
+        // autoplayTimeout: 3000,
+        dots: true,
+        merge: true,
+        loop: true,
+        animateOut: 'fadeOut',
+        smartSpeed: 700,
         center: true
     });
 
@@ -65,4 +106,25 @@ $(function() {
         mobileMenu.removeClass('open');
         bodyMask.removeClass('show');
     });
+
+    // Get the modal
+    var modal = document.getElementById('videoModal');
+    // Get the button that opens the modal
+    var btn = document.getElementById("openButton");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close");
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
